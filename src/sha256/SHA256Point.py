3381264b4ec00b7f58b06ce0243289b3e4810779
gil7788 <gil7788@gmail.com>
2023-11-15 22:07:58 +0200

    Implement Encode and Parse SHA256Point Class
    - Fix Scalar Multiplication

--- src/sha256/SHA256Point.py
+++ src/sha256/SHA256Point.py
@@ -28,9 +28,40 @@ class SHA256Point(EllipticCurvePoint):
         total = u * self.get_g() + v * self
         return total.x.value == sig.r
 
+    def sec(self, compressed=True):
+        # returns the binary version of the SEC format
+        if compressed:
+            if self.y.value % 2 == 0:
+                return b'\x02' + self.x.value.to_bytes(32, 'big')
+            else:
+                return b'\x03' + self.x.value.to_bytes(32, 'big')
+        else:
+            return b'\x04' + self.x.value.to_bytes(32, 'big') + self.y.value.to_bytes(32, 'big')
+
+    @classmethod
+    def parse(cls, sec_bin):
+        if sec_bin[0] == 4:
+            x = int.from_bytes(sec_bin[1:33], 'big')
+            y = int.from_bytes(sec_bin[33:65], 'big')
+            return SHA256Point(x, y)
+        is_even = sec_bin[0] == 2
+        x = SHA256Element(int.from_bytes(sec_bin[1:], 'big'))
+        alpha = x**3 + SHA256Element(secp.A) * x + SHA256Element(secp.B)
+        beta = alpha.sqrt()
+        if beta.value % 2 == 0:
+            even_beta = beta
+            odd_beta = SHA256Element(secp.PRIME - beta.value)
+        else:
+            even_beta = SHA256Element(secp.PRIME - beta.value)
+            odd_beta = beta
+        if is_even:
+            return SHA256Point(x, even_beta)
+        else:
+            return SHA256Point(x, odd_beta)
+
     def __rmul__(self, coefficient):
-        coefficient = coefficient % secp.N
-        return super().__rmul__(coefficient)
+        elliptic_curve_point = super().__rmul__(coefficient)
+        return SHA256Point(elliptic_curve_point.x, elliptic_curve_point.y)
 
     def __repr__(self):
         if self.x is None:

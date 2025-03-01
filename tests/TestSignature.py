3e0c72fe30b4bcec7c050cfe7a716993e418fae8
gil7788 <gil7788@gmail.com>
2025-03-01 15:36:40 +0200

    Fix Signature DER
    * Add test for DER

--- tests/TestSignature.py
+++ tests/TestSignature.py
@@ -17,3 +17,17 @@ class TestSignature(unittest.TestCase):
             sig2 = Signature.parse(der)
             self.assertEqual(sig2.r, r)
             self.assertEqual(sig2.s, s)
+
+    def test_specific_der(self):
+        self.assertEqual(0,0)
+        r = 0x37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6
+        s = 0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
+        #
+        sig = Signature(r, s)
+        der = sig.der()
+
+        # validate that decoding works well
+        sig2 = Signature.parse(der)
+
+        self.assertEqual(der.hex(), "3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6022100\
+8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec")
